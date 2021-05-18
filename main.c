b8e1d89fae0484aebd4aa07acd96fca8149dc129
Adrien CASTELBOU <acastelb@e2r7p2.42.fr>
2021-05-18 11:12:15 +0200

    add stacks, first steps checkinput

--- main.c
+++ main.c
@@ -6,12 +6,91 @@
 /*   By: acastelb <acastelb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 10:38:02 by acastelb          #+#    #+#             */
-/*   Updated: 2021/05/18 10:41:31 by acastelb         ###   ########.fr       */
+/*   Updated: 2021/05/18 11:11:31 by acastelb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int ac, char **argv)
+#include "push_swap.h"
+
+t_stacks	*ft_stacksnew(void)
 {
+	t_stacks	*stacks;
+
+	if (!(stacks = malloc(sizeof(t_stacks))))
+		return (NULL);
+	stacks->a = NULL;
+	stacks->b = NULL;
+	return (stacks);
+}
+
+long int	ft_atoli(const char *str)
+{
+	int			i;
+	int			is_neg;
+	long int	result;
+
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	is_neg = 1;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			is_neg *= -1;
+		i++;
+	}
+	result = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * is_neg);
+}
+
+void	ft_stacksclear(t_stacks *stacks)
+{
+	if (!stacks)
+		return ;
+	ft_lstclear(&(stacks->a), free);
+	ft_lstclear(&(stacks->b), free);
+	free(stacks);
+	stacks = NULL;
+}
+
+int		check_argvs(t_stacks *stacks, char **av)
+{
+	int	i;
+	int	ret;
+
+	ret = 1;
+	i = 0;
+	while (av[++i])
+	{
+		ret = get_av_elem(stacks, av[i]);
+		if (ret == 0)
+			break ;
+	}
+	if (ret == 0)
+		ft_stacksclear(stacks);
+	return (ret);
+}
+
+int		main(int ac, char **argv)
+{
+	t_stacks	*stacks;
+
+	stacks = NULL;
 	if (ac == 1)
 		return(0);
+	if (!(stacks = ft_stacksnew()))
+	{
+		ft_putstr_fd("Error\n", STDERR_FILENO);
+		return (1);
+	}
+	if (!check_argvs(stacks, argv))
+	{
+		ft_putstr_fd("Error\n", STDERR_FILENO);
+		return (1);
+	}
 }
